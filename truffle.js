b15be196d6440c317a51e1067100bf6aa91cb42f
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-22 18:35:13 +0530

    first commit

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "canvas antique alley bounce donkey plug bargain noodle razor release sign acid";
 
 module.exports = {
   networks: {
@@ -8,7 +8,7 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 6000000
     }
   },
   compilers: {
